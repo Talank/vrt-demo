52fb27fd9aeec37842753e15b91ecc9b044d0ace
talank <talank@gces.edu.np>
2021-02-28 18:18:54 +0545

    added nightwatch vrt setup

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,5 +1,17 @@
-module.exports = {
-    src_folders: ['test'],
+const path = require('path')
+
+function generateScreenshotFilePath(nightwatchClient, basePath, imagePath) {
+    return path.join(process.cwd(), basePath, imagePath)
+}
+
+const config = {
+    custom_commands_path: [
+        'node_modules/nightwatch-vrt/commands'
+    ],
+    custom_assertions_path: [
+        'node_modules/nightwatch-vrt/assertions'
+    ],
+    src_folders: ['tests'],
     test_settings: {
         default: {
             selenium_host: '127.0.0.1',
@@ -12,7 +24,21 @@ module.exports = {
                     args: ['disable-gpu'],
                     w3c: false
                 }
+            },
+            visual_regression_settings: {
+                generate_screenshot_path: generateScreenshotFilePath,
+                latest_screenshots_path: 'tests/vrt/latest',
+                latest_suffix: '',
+                baseline_screenshots_path: 'tests/vrt/baseline',
+                baseline_suffix: '',
+                diff_screenshots_path: 'tests/vrt/diff',
+                diff_suffix: '',
+                threshold: 0,
+                prompt: false,
+                always_save_diff_screenshot: false
             }
         }
     }
 }
+
+module.exports = config
